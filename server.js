c2166995735abc0bb90cc795999eb14146d15bc3
skarkera <sabdakarkera@sabdas-mbp.lan>
2020-10-04 23:52:10 -0400

    server changes

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
+//Budget API
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3002;
 
-app.use('/', express.static('public'));
+//app.use('/', express.static('public'));
+
+app.use(cors());
 
 const budget = require("./budget-data.json");
-console.log('My budget is', budget);
+//console.log('My budget is', budget);
 
-app.get('/hello', (req, res) => {
+/*app.get('/hello', (req, res) => {
     res.send('Hello World!');
-});
+});*/
 
 app.get('/budget', (req, res) => {
     res.send(budget);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+    console.log(`API served at http://localhost:${port}`);
 });
 
